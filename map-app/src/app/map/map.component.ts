6d699cb766aa97aa5d5bc6f95929fe76698f9521
Angela Wilson <awilson1233@yahoo.com>
2019-06-04 16:53:36 -0700

    code linting and removed console logs

--- map-app/src/app/map/map.component.ts
+++ map-app/src/app/map/map.component.ts
@@ -23,11 +23,11 @@ export class MapComponent implements OnInit {
   // Set placeholders for data overlay from buildings
   uniqBldgClasses: any;
   colors = {
-    'Completely Destroyed': "#bf0e00",
-    'Highly Damaged': "#c96900",
-    'Moderately Damaged': "#c8a400",
-    'Negligible to slight damage': "#8bc500",
-    'Unknown': "#666666"
+    'Completely Destroyed': '#bf0e00',
+    'Highly Damaged': '#c96900',
+    'Moderately Damaged': '#c8a400',
+    'Negligible to slight damage': '#8bc500',
+    'Unknown': '#666666'
 
   };
 
@@ -37,13 +37,11 @@ export class MapComponent implements OnInit {
   // Process data from db - not used
   ngOnInit() {
     this.getBuildings().subscribe((buildings: any) => {
-      console.log(buildings, 'buildings')
-      const bldg_damage = Object.keys(this.colors);
-      this.uniqBldgClasses = this.uniq(bldg_damage);
-      console.log(this.uniqBldgClasses, 'uniqBldgClasses')
+      const bldgDamage = Object.keys(this.colors);
+      this.uniqBldgClasses = this.uniq(bldgDamage);
       this.layers = buildings.map((b) => {
 
-        return circle([b.lat, b.lng], { color: this.colors[b.main_damage || "Unknown"], radius: 50 })
+        return circle([b.lat, b.lng], { color: this.colors[b.main_damage || 'Unknown'], radius: 50 })
           .bindPopup(
             '<p>_id: ' + b._id + '</p>' +
             '<p>Class: ' + b.main_damage + '</p>' +
@@ -61,8 +59,8 @@ export class MapComponent implements OnInit {
 
   // Method to retrieve unique values of array
   uniq(a) {
-    var seen = {};
-    return a.filter(function(item) {
+    const seen = {};
+    return a.filter(item => {
       return seen.hasOwnProperty(item) ? false : (seen[item] = true);
     });
   }
@@ -70,27 +68,26 @@ export class MapComponent implements OnInit {
   // Method to add requested geojson layer to map
   onMapReady(map: Map) {
     this.http.get('assets/Peak_Gust_mph.geojson').subscribe((json: any) => {
-      console.log(json);
       geoJSON(json, {
-        style: function(feature) {
+        style: feature => {
           switch (feature.properties.Name) {
-            case '40': return { color: "#00d4e5" , opacity: 0.0, fillOpacity: 0.05};
-            case '50': return { color: "#00d4e5" , opacity: 0.0, fillOpacity: 0.05};
-            case '60': return { color: "#00e1ba" , opacity: 0.0, fillOpacity: 0.05};
-            case '70': return { color: "#00e1ba" , opacity: 0.0, fillOpacity: 0.05};
-            case '80': return { color: "#00de7f" , opacity: 0.0, fillOpacity: 0.05};
-            case '90': return { color: "#00db0f" , opacity: 0.0, fillOpacity: 0.05};
-            case '100': return { color: "#25d500" , opacity: 0.0, fillOpacity: 0.05};
-            case '110': return { color: "#59d200" , opacity: 0.0, fillOpacity: 0.05};
-            case '120': return { color: "#8bce00" , opacity: 0.0, fillOpacity: 0.05};
-            case '130': return { color: "#bccb00" , opacity: 0.0, fillOpacity: 0.05};
-            case '140': return { color: "#c8a500" , opacity: 0.0, fillOpacity: 0.05};
-            case '150': return { color: "#c46900" , opacity: 0.0, fillOpacity: 0.05};
-            case '160': return { color: "#c46900" , opacity: 0.0, fillOpacity: 0.05};
-            case '170': return { color: "#c13a00" , opacity: 0.0, fillOpacity: 0.05};
-            case '180': return { color: "#c13a00" , opacity: 0.0, fillOpacity: 0.05};
-            case '190': return { color: "#bf0e00" , opacity: 0.0, fillOpacity: 0.05};
-            case '200': return { color: "#bf0e00" , opacity: 0.0, fillOpacity: 0.05};
+            case '40': return { color: '#00d4e5' , opacity: 0.0, fillOpacity: 0.05};
+            case '50': return { color: '#00d4e5' , opacity: 0.0, fillOpacity: 0.05};
+            case '60': return { color: '#00e1ba' , opacity: 0.0, fillOpacity: 0.05};
+            case '70': return { color: '#00e1ba' , opacity: 0.0, fillOpacity: 0.05};
+            case '80': return { color: '#00de7f' , opacity: 0.0, fillOpacity: 0.05};
+            case '90': return { color: '#00db0f' , opacity: 0.0, fillOpacity: 0.05};
+            case '100': return { color: '#25d500' , opacity: 0.0, fillOpacity: 0.05};
+            case '110': return { color: '#59d200' , opacity: 0.0, fillOpacity: 0.05};
+            case '120': return { color: '#8bce00' , opacity: 0.0, fillOpacity: 0.05};
+            case '130': return { color: '#bccb00' , opacity: 0.0, fillOpacity: 0.05};
+            case '140': return { color: '#c8a500' , opacity: 0.0, fillOpacity: 0.05};
+            case '150': return { color: '#c46900' , opacity: 0.0, fillOpacity: 0.05};
+            case '160': return { color: '#c46900' , opacity: 0.0, fillOpacity: 0.05};
+            case '170': return { color: '#c13a00' , opacity: 0.0, fillOpacity: 0.05};
+            case '180': return { color: '#c13a00' , opacity: 0.0, fillOpacity: 0.05};
+            case '190': return { color: '#bf0e00' , opacity: 0.0, fillOpacity: 0.05};
+            case '200': return { color: '#bf0e00' , opacity: 0.0, fillOpacity: 0.05};
           }
         }
       }).addTo(map);
