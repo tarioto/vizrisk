fe76bea3c95c1d9135447e84804014014c846782
Tim Arioto <timarioto@gmail.com>
2020-09-19 11:29:14 +0200

    remove leaflet

--- map-app/src/app/app.module.ts
+++ map-app/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { MapComponent } from './map/map.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -62,7 +61,6 @@ import { SummaryTableComponent } from './map/summary-table/summary-table.compone
     BrowserAnimationsModule,
     MatCheckboxModule,
     HttpClientModule,
-    LeafletModule.forRoot(),
     NgxMapboxGLModule.withConfig({
      accessToken: 'pk.eyJ1IjoidGltb3RoeS1hcmlvdG8iLCJhIjoiY2p3aWw3eTIxMDBobTRha3M2MGZ0b2t4NiJ9.PPEqq6_WNM2ngRwvSf9p2g'
      // geocoderAccessToken: 'TOKEN'
